742ba69719b014d12913cc037e0f7cff8163dee8
mlpierce22 <mlpierce22@gmail.com>
2020-08-13 16:24:20 -0700

    create quote intro page

---
+++ src/components/TheQuoteIntroPage.vue
@@ -0,0 +1,28 @@
+<template lang="html">
+  <div class="the-quote-intro-page">
+    <div class="number-inputs" v-for="(value, key) in formData" :key="key">
+      <VPromptedNumberInput :inputData="value" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from "vue-property-decorator";
+import { QuoteIntroForm } from "@/models";
+import VPromptedNumberInput from "@/components/shared/VPromptedNumberInput.vue";
+
+@Component({
+  components: {
+    VPromptedNumberInput
+  }
+})
+export default class TheQuoteIntroPage extends Vue {
+  @Prop({ required: true })
+  private formData!: QuoteIntroForm;
+}
+</script>
+
+<style scoped lang="scss">
+.the-quote-intro-page {
+}
+</style>
