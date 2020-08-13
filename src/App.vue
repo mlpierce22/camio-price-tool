1204b75f884b1407fffa690d957c439187226cfe
mlpierce22 <mlpierce22@gmail.com>
2020-08-13 16:27:55 -0700

    add data to app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,67 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
     <v-main>
-      <HelloWorld />
+      <div class="app-container">
+        <TheQuoteIntroPage
+          v-if="formProgressionState.onStep === 0"
+          :formData="quoteIntroPageFormData"
+        />
+        <!-- Otherwise: stepper -->
+        <div class="seperator"></div>
+      </div>
     </v-main>
   </v-app>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import TheQuoteIntroPage from "@/components/TheQuoteIntroPage.vue";
+import { QuoteIntroForm } from "@/models";
 
 export default Vue.extend({
-  name: "App",
-
   components: {
-    HelloWorld
+    TheQuoteIntroPage
   },
 
   data: () => ({
-    //
-  })
+    formProgressionState: {
+      furthestStep: 0, // Options for steps 0 (intro), 1 (account), 2 (plans), 3 (locations), 4 (done)
+      onStep: 0,
+      showLocations: true
+    },
+    quoteIntroPageFormData: {
+      numCameras: {
+        prompt: "How many cameras do you have?",
+        units: "Camera",
+        initVal: 10
+      },
+      numLANLocations: {
+        prompt: "Across how many LAN locations are your cameras located?",
+        units: "Location",
+        initVal: 1
+      }
+    } as QuoteIntroForm,
+    accountPageFormData: {},
+    createPlansPageFormData: {},
+    addLocationsPageFormData: {}
+  }),
+  methods: {}
 });
 </script>
+
+<style scoped lang="scss">
+.app-container {
+  display: flex;
+  border: 3px solid #f7931e;
+  border-radius: 20px;
+  margin: 30px 10.3%;
+  padding: 40px 100px;
+
+  .seperator {
+    border: 3px solid #f7931e;
+    height: 3px;
+    margin: -100px;
+    width: 100%;
+  }
+}
+</style>
