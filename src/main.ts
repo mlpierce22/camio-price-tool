fa3933fe2a8594d67e6abd21d65706d71c436e7d
mlpierce22 <mlpierce22@gmail.com>
2020-08-13 10:52:57 -0700

    install material (less compression, smaller dep)

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
 import Vue from "vue";
 import App from "./App.vue";
+import { MdButton } from "vue-material/dist/components";
+import "vue-material/dist/vue-material.min.css";
+import "vue-material/dist/theme/default.css";
+
+Vue.use(MdButton);
 
 Vue.config.productionTip = false;
 
