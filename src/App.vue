8f587dd9facc09c2cdd4063e6a1ea45d5933b085
mlpierce22 <mlpierce22@gmail.com>
2020-08-14 13:20:30 -0700

    update app to handle emit events

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,13 @@
         <TheQuoteIntroPage
           v-if="formProgressionState.onStep === 0"
           :formData="quoteIntroPageFormData"
+          @new-value-keyed="updateValue($event)"
         />
         <!-- Otherwise: stepper -->
+
         <div class="seperator"></div>
+
+        <!-- <VNextBackButton :buttonsConfig="" @nextStep="" /> -->
       </div>
     </v-main>
   </v-app>
@@ -16,10 +20,12 @@
 <script lang="ts">
 import Vue from "vue";
 import TheQuoteIntroPage from "@/components/TheQuoteIntroPage.vue";
-import { QuoteIntroForm } from "@/models";
+import VNextBackButton from "@/components/shared/VBackNextButton.vue";
+import { QuoteIntroForm, BackNextButtonConfig } from "@/models";
 
 export default Vue.extend({
   components: {
+    // VNextBackButton,
     TheQuoteIntroPage
   },
 
@@ -27,40 +33,119 @@ export default Vue.extend({
     formProgressionState: {
       furthestStep: 0, // Options for steps 0 (intro), 1 (account), 2 (plans), 3 (locations), 4 (done)
       onStep: 0,
+      maxStep: 4,
       showLocations: true
     },
+    // initButtonConfig: {
+    //   nextText: "Account-wide",
+    //   backText: "Back",
+    //   showBack: false,
+    //   showNext: true
+    // } as BackNextButtonConfig,
     quoteIntroPageFormData: {
       numCameras: {
         prompt: "How many cameras do you have?",
         units: "Camera",
-        initVal: 10
+        value: 10
       },
       numLANLocations: {
         prompt: "Across how many LAN locations are your cameras located?",
         units: "Location",
-        initVal: 1
+        value: 1
       }
     } as QuoteIntroForm,
     accountPageFormData: {},
     createPlansPageFormData: {},
     addLocationsPageFormData: {}
   }),
-  methods: {}
+  methods: {
+    updateValue: function(newValObj) {
+      const castVal = parseInt(newValObj.newVal, 10);
+      this.quoteIntroPageFormData[newValObj.key].value = castVal;
+    }
+    // updateButtonConfig: function(step) {
+    //   switch (step) {
+    //     case 0:
+    //       return {
+    //         nextText: "Account-wide",
+    //         backText: "Back to Intro",
+    //         showBack: false,
+    //         showNext: true
+    //       };
+    //     case 1:
+    //       return {
+    //         nextText: "Create Plans",
+    //         backText: "Back to Account",
+    //         showBack: true,
+    //         showNext: true
+    //       };
+    //     case 2:
+    //       return {
+    //         nextText: this.formProgressionState.maxStep == 4 ? "Add Locations",
+    //         backText: "Back to Plans",
+    //         showBack: true,
+    //         showNext: true
+    //       };
+    //     case 3 && this.formProgressionState.maxStep == 4:
+    //       // then location
+    //       return {
+    //         nextText: "Next",
+    //         backText: "Back",
+    //         showBack: true,
+    //         showNext: true
+    //       };
+    //     case 3 && this.formProgressionState.maxStep == 3:
+    //       // then done
+    //       return {
+    //         nextText: "Next",
+    //         backText: "Back",
+    //         showBack: false,
+    //         showNext: false
+    //       };
+    //     case 4:
+    //       // then done
+    //       return {
+    //         nextText: "Next",
+    //         backText: "Back",
+    //         showBack: false,
+    //         showNext: false
+    //       };
+    //   }
+    //   if (step === 0) {
+    //     this.initButtonConfig;
+    //   }
+    //   e;
+    //   if (step === this.formProgressionState.maxStep) {
+    //   }
+    // },
+    // nextStep: function() {
+    //   this.formProgressionState.onStep++;
+    // },
+    // prevStep: function() {
+    //   this.formProgressionState.onStep--;
+    // }
+  }
 });
 </script>
 
 <style scoped lang="scss">
 .app-container {
   display: flex;
+  flex-direction: column;
   border: 3px solid #f7931e;
   border-radius: 20px;
   margin: 30px 10.3%;
-  padding: 40px 100px;
+  padding: 40px 40px 40px 100px;
+  align-items: center;
+
+  @media only screen and (max-width: 600px) {
+    padding: 40px;
+  }
 
   .seperator {
     border: 3px solid #f7931e;
     height: 3px;
-    margin: -100px;
+    //margin: -100px;
     width: 100%;
   }
 }
