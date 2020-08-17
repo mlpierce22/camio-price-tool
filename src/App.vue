62639f8080f1a4bc421f52abdbfca627f488f789
mlpierce22 <mlpierce22@gmail.com>
2020-08-17 11:40:56 -0700

    finish first version of stepper

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,54 @@
 <template>
   <v-app>
     <v-main>
-      <div class="app-container">
-        <TheQuoteIntroPage
+      <!-- <div class="app-container"> -->
+      <v-stepper
+        :alt-labels="true"
+        v-model="stepperData.progressionState.onStep"
+        class="app-container"
+        v-resize="checkOrientation"
+        :vertical="stepperData.isVertical"
+      >
+        <v-stepper-header v-if="stepperData.progressionState.furthestStep != 0">
+          <template class="app-steps" v-for="(step, index) in dynamicSlides">
+            <v-divider v-if="index !== 0" :key="`${index}`"></v-divider>
+            <v-stepper-step
+              :key="`${index}-step`"
+              :step="step.stepNumber"
+              :complete="
+                step.stepIndex < stepperData.progressionState.furthestStep
+              "
+              :editable="
+                step.stepIndex < stepperData.progressionState.furthestStep
+              "
+            >
+              {{ step.stepName }}
+            </v-stepper-step>
+          </template>
+        </v-stepper-header>
+        <v-stepper-items class="app-content">
+          <template v-for="(step, index) in dynamicSlides">
+            <v-stepper-content
+              :key="`${index}-content`"
+              :step="step.stepNumber"
+            >
+              step: {{ step }}
+              <button @click="nextStep()">Next</button>
+              <button @click="prevStep()">Back</button>
+            </v-stepper-content>
+          </template>
+        </v-stepper-items>
+      </v-stepper>
+      <!-- <div class="seperator"></div> -->
+      <!-- <TheQuoteIntroPage
           v-if="formProgressionState.onStep === 0"
           :formData="quoteIntroPageFormData"
           @new-value-keyed="updateQuotePageVals($event)"
-        />
-        <!-- Otherwise: stepper -->
+        /> -->
+      <!-- Otherwise: stepper -->
 
-        <div class="seperator"></div>
-
-        <!-- <VNextBackButton :buttonsConfig="" @nextStep="" /> -->
-      </div>
+      <!-- <VNextBackButton :buttonsConfig="" @nextStep="" /> -->
+      <!-- </div> -->
     </v-main>
   </v-app>
 </template>
@@ -26,10 +62,54 @@ import { QuoteIntroForm, BackNextButtonConfig } from "@/models";
 export default Vue.extend({
   components: {
     // VNextBackButton,
-    TheQuoteIntroPage
+    //TheQuoteIntroPage
   },
 
   data: () => ({
+    stepperData: {
+      orientationThreshold: 600,
+      isVertical: false,
+      progressionState: {
+        // NOTE: None of these are indexes
+        furthestStep: 0,
+        onStep: 1,
+        maxStep: 5,
+        showLocations: true,
+        locationStep: 4
+      },
+      steps: [
+        {
+          stepNumber: 1,
+          stepIndex: 0,
+          stepName: "Introduction",
+          instance: ""
+        },
+        {
+          stepNumber: 2,
+          stepIndex: 1,
+          stepName: "Account",
+          instance: ""
+        },
+        {
+          stepNumber: 3,
+          stepIndex: 2,
+          stepName: "Plans",
+          instance: ""
+        },
+        {
+          stepNumber: 4,
+          stepIndex: 3,
+          stepName: "Locations",
+          instance: ""
+        },
+        {
+          stepNumber: 5,
+          stepIndex: 4,
+          stepName: "Review",
+          instance: ""
+        }
+      ]
+    },
     formProgressionState: {
       furthestStep: 0, // Options for steps 0 (intro), 1 (account), 2 (plans), 3 (locations), 4 (done)
       onStep: 0,
@@ -51,17 +131,70 @@ export default Vue.extend({
       numLANLocations: {
         prompt: "Across how many LAN locations are your cameras located?",
         units: "Location",
-        value: 1
+        value: 2
       }
     } as QuoteIntroForm,
     accountPageFormData: {},
     createPlansPageFormData: {},
     addLocationsPageFormData: {}
   }),
+  computed: {
+    dynamicSlides: function() {
+      if (this.stepperData.progressionState.showLocations === false) {
+        const firstSteps: {}[] = this.stepperData.steps.slice(
+          0,
+          this.stepperData.progressionState.locationStep - 1
+        );
+        const lastSteps = this.stepperData.steps
+          .slice(
+            this.stepperData.progressionState.locationStep,
+            this.stepperData.steps.length
+          )
+          .map((arrItem, indx) => {
+            arrItem.stepNumber =
+              this.stepperData.progressionState.locationStep + indx;
+
+            return arrItem;
+          });
+        return [].concat(firstSteps as [], lastSteps as []);
+      } else {
+        return this.stepperData.steps;
+      }
+    }
+  },
   methods: {
+    checkOrientation: function() {
+      this.stepperData.isVertical =
+        window.innerWidth <= this.stepperData.orientationThreshold;
+    },
     updateQuotePageVals: function(newValObj) {
       const castVal = parseInt(newValObj.newVal, 10);
       this.quoteIntroPageFormData[newValObj.key].value = castVal;
+    },
+    nextStep: function() {
+      // If we are on the first step, we need to decide whether or not to show locations
+      if (this.quoteIntroPageFormData["numLANLocations"].value === 1) {
+        this.stepperData.progressionState.showLocations = false;
+      } else {
+        this.stepperData.progressionState.showLocations = true;
+      }
+
+      // Update the current step as long as it is less than or equal to the maxStep
+      if (
+        this.stepperData.progressionState.onStep + 1 <=
+        this.stepperData.progressionState.maxStep
+      ) {
+        this.stepperData.progressionState.onStep++;
+      }
+      this.stepperData.progressionState.furthestStep = Math.max(
+        this.stepperData.progressionState.onStep,
+        this.stepperData.progressionState.furthestStep
+      );
+    },
+    prevStep: function() {
+      if (this.stepperData.progressionState.onStep - 1 >= 1) {
+        this.stepperData.progressionState.onStep--;
+      }
     }
     // updateButtonConfig: function(step) {
     //   switch (step) {
@@ -118,12 +251,6 @@ export default Vue.extend({
     //   if (step === this.formProgressionState.maxStep) {
     //   }
     // },
-    // nextStep: function() {
-    //   this.formProgressionState.onStep++;
-    // },
-    // prevStep: function() {
-    //   this.formProgressionState.onStep--;
-    // }
   }
 });
 </script>
@@ -135,11 +262,17 @@ export default Vue.extend({
   border: 3px solid #f7931e;
   border-radius: 20px;
   margin: 30px 10.3%;
-  padding: 40px 40px 40px 100px;
-  align-items: center;
 
-  @media only screen and (max-width: 600px) {
-    padding: 40px;
+  .v-stepper__header {
+    box-shadow: none;
+  }
+
+  .app-content {
+    padding: 40px 40px 40px 100px;
+
+    @media only screen and (max-width: 650px) {
+      padding: 40px;
+    }
   }
 
   .seperator {
