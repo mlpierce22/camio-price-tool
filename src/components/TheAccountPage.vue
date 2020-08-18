a0fb7691d0326338b8f5320ff02b8ed0d77749e8
mlpierce22 <mlpierce22@gmail.com>
2020-08-18 15:53:36 -0700

    Generate the account page

---
+++ src/components/TheAccountPage.vue
@@ -0,0 +1,36 @@
+<!----------------- BEGIN JS/TS ------------------->
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+@Component({
+  components: {}
+})
+export default class TheAccountPage extends Vue {
+  // ---------- Props ----------
+
+  // ------- Local Vars --------
+
+  // --------- Watchers --------
+
+  // ------- Lifecycle ---------
+  constructor() {
+    super();
+  }
+  // --------- Methods ---------
+}
+</script>
+<!----------------- END JS/TS --------------------->
+
+<!----------------- BEGIN HTML -------------------->
+<template lang="html">
+  <div class="the-account-page">
+    <h1>the-account-page component works!</h1>
+  </div>
+</template>
+<!----------------- END HTML ---------------------->
+
+<!----------------- BEGIN CSS/SCSS ---------------->
+<style scoped lang="scss">
+.the-account-page {
+}
+</style>
+<!----------------- END CSS/SCSS ------------------>
