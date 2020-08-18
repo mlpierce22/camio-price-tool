6c45593301e48577a8373800703ace4fbc882acb
mlpierce22 <mlpierce22@gmail.com>
2020-08-18 15:58:35 -0700

    Generate the create plans modal

---
+++ src/components/TheCreatePlansModal.vue
@@ -0,0 +1,43 @@
+<!----------------- BEGIN JS/TS ------------------->
+<script lang="ts">
+
+import { Component, Vue } from "vue-property-decorator";
+@Component({
+  components: {}
+})
+export default class TheCreatePlansModal extends Vue {
+
+  // ---------- Props ----------
+
+  // ------- Local Vars --------
+
+  // --------- Watchers --------
+
+  // ------- Lifecycle ---------
+  constructor() {
+    super();
+  }
+  // --------- Methods ---------
+
+}
+
+</script>
+<!----------------- END JS/TS --------------------->
+
+<!----------------- BEGIN HTML -------------------->
+<template lang="html">
+
+  <div class="the-create-plans-modal">
+    <h1>the-create-plans-modal component works!</h1>
+  </div>
+
+</template>
+<!----------------- END HTML ---------------------->
+
+<!----------------- BEGIN CSS/SCSS ---------------->
+<style scoped lang="scss">
+  .the-create-plans-modal {
+
+  }
+</style>
+<!----------------- END CSS/SCSS ------------------>
