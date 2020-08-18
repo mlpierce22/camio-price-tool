c9bac34621e314ef0c780d063a93d5e960d4413f
mlpierce22 <mlpierce22@gmail.com>
2020-08-18 15:58:45 -0700

    Generate the estimate page

---
+++ src/components/TheEstimatePage.vue
@@ -0,0 +1,43 @@
+<!----------------- BEGIN JS/TS ------------------->
+<script lang="ts">
+
+import { Component, Vue } from "vue-property-decorator";
+@Component({
+  components: {}
+})
+export default class TheEstimatePage extends Vue {
+
+  // ---------- Props ----------
+
+  // ------- Local Vars --------
+
+  // --------- Watchers --------
+
+  // ------- Lifecycle ---------
+  constructor() {
+    super();
+  }
+  // --------- Methods ---------
+
+}
+
+</script>
+<!----------------- END JS/TS --------------------->
+
+<!----------------- BEGIN HTML -------------------->
+<template lang="html">
+
+  <div class="the-estimate-page">
+    <h1>the-estimate-page component works!</h1>
+  </div>
+
+</template>
+<!----------------- END HTML ---------------------->
+
+<!----------------- BEGIN CSS/SCSS ---------------->
+<style scoped lang="scss">
+  .the-estimate-page {
+
+  }
+</style>
+<!----------------- END CSS/SCSS ------------------>
