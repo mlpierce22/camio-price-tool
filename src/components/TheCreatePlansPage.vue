f169d8c9d8149aaf51df3d1e024ae684d54a769c
mlpierce22 <mlpierce22@gmail.com>
2020-08-18 15:56:02 -0700

    generate create plans page

---
+++ src/components/TheCreatePlansPage.vue
@@ -0,0 +1,43 @@
+<!----------------- BEGIN JS/TS ------------------->
+<script lang="ts">
+
+import { Component, Vue } from "vue-property-decorator";
+@Component({
+  components: {}
+})
+export default class TheCreatePlansPage extends Vue {
+
+  // ---------- Props ----------
+
+  // ------- Local Vars --------
+
+  // --------- Watchers --------
+
+  // ------- Lifecycle ---------
+  constructor() {
+    super();
+  }
+  // --------- Methods ---------
+
+}
+
+</script>
+<!----------------- END JS/TS --------------------->
+
+<!----------------- BEGIN HTML -------------------->
+<template lang="html">
+
+  <div class="the-create-plans-page">
+    <h1>the-create-plans-page component works!</h1>
+  </div>
+
+</template>
+<!----------------- END HTML ---------------------->
+
+<!----------------- BEGIN CSS/SCSS ---------------->
+<style scoped lang="scss">
+  .the-create-plans-page {
+
+  }
+</style>
+<!----------------- END CSS/SCSS ------------------>
