e3ef01790ae76b500978ca4cbead9a50eff7a62b
mlpierce22 <mlpierce22@gmail.com>
2020-08-18 15:04:51 -0700

    generated back next button component

---
+++ src/components/shared/VBackNextButton.vue
@@ -0,0 +1,30 @@
+<!----------------- BEGIN JS/TS ----------------->
+<script lang="ts">
+
+import { Component, Vue } from "vue-property-decorator";
+@Component({
+  components: {}
+})
+export default class VBackNextButton extends Vue {
+  constructor() {
+    super();
+  }
+}
+
+</script>
+
+<!----------------- BEGIN HTML ----------------->
+<template lang="html">
+
+  <section class="v-back-next-button">
+    <h1>v-back-next-button component works!</h1>
+  </section>
+
+</template>
+
+<!----------------- BEGIN CSS/SCSS ----------------->
+<style scoped lang="scss">
+  .v-back-next-button {
+
+  }
+</style>
