c0a030451a618d57aec212facdfffd5ea051e916
mlpierce22 <mlpierce22@gmail.com>
2020-08-17 17:52:17 -0700

    get rid of theme in vuetify (since it doesnt work)

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,16 +1,6 @@
 import Vue from "vue";
 import Vuetify from "vuetify/lib";
 
-Vue.use(Vuetify, {
-  theme: {
-    themes: {
-      light: {
-        primary: "#f7931e",
-        secondary: "#50B536",
-        error: "#FF0000"
-      }
-    }
-  }
-});
+Vue.use(Vuetify);
 
 export default new Vuetify({});
