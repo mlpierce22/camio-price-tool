8772f2a75b8273f6e9fcc9b35b9221fddb5e1bdf
mlpierce22 <mlpierce22@gmail.com>
2020-08-20 14:36:40 -0700

    styling and data binding for vbutton group

--- src/components/shared/form_items/VButtonGroup.vue
+++ src/components/shared/form_items/VButtonGroup.vue
@@ -1,19 +1,29 @@
 <!----------------- BEGIN JS/TS ------------------->
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
+import { Component, Vue, Prop, Watch } from "vue-property-decorator";
+import { AccountForm, Indexing, AccountSubForm } from "@/models";
 @Component({
   components: {}
 })
 export default class VButtonGroup extends Vue {
   // ---------- Props ----------
+  @Prop() data!: AccountForm;
+
+  @Prop() selected!: number;
 
   // ------- Local Vars --------
 
+  selectedLocal: number;
   // --------- Watchers --------
 
+  @Watch("selectedLocal")
+  selectedChanged() {
+    this.$emit("selected-changed", this.selectedLocal);
+  }
   // ------- Lifecycle ---------
   constructor() {
     super();
+    this.selectedLocal = this.selected;
   }
   // --------- Methods ---------
 }
@@ -23,7 +33,22 @@ export default class VButtonGroup extends Vue {
 <!----------------- BEGIN HTML -------------------->
 <template lang="html">
   <div class="v-button-group">
-    <h1>v-button-group component works!</h1>
+    <slot></slot>
+    <div class="sub-prompt">{{ data.subPrompt }}</div>
+    <v-btn-toggle
+      v-model="selectedLocal"
+      :mandatory="true"
+      color="primary"
+      :rounded="true"
+      active-class="active"
+    >
+      <v-btn
+        v-for="(option, index) in data.selectionOpts"
+        :key="`${index}-button-group-item`"
+      >
+        <div class="btn-text">{{ option }}</div>
+      </v-btn>
+    </v-btn-toggle>
   </div>
 </template>
 <!----------------- END HTML ---------------------->
@@ -31,6 +56,35 @@ export default class VButtonGroup extends Vue {
 <!----------------- BEGIN CSS/SCSS ---------------->
 <style scoped lang="scss">
 .v-button-group {
+  display: flex;
+  flex-direction: column;
+
+  .sub-prompt {
+    margin-bottom: 10px;
+  }
+
+  ::v-deep .v-btn-toggle {
+    .v-btn.v-btn {
+      opacity: 1;
+
+      &.v-size--default {
+        border: solid 3px;
+        border-color: #f7931e !important;
+        margin: -1.5px;
+        background-color: white;
+        color: #f7931e;
+        font-weight: bold;
+      }
+      &.active {
+        background: #f7931e !important;
+        color: white;
+
+        &::before {
+          opacity: 0;
+        }
+      }
+    }
+  }
 }
 </style>
 <!----------------- END CSS/SCSS ------------------>
