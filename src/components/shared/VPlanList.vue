9812943c752b5a2a8fd293b86f4398327b6bd28a
mlpierce22 <mlpierce22@gmail.com>
2020-08-24 14:33:45 -0700

    create plan list.vue

---
+++ src/components/shared/VPlanList.vue
@@ -0,0 +1,36 @@
+<!----------------- BEGIN JS/TS ------------------->
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+@Component({
+  components: {}
+})
+export default class VPlanList extends Vue {
+  // ---------- Props ----------
+
+  // ------- Local Vars --------
+
+  // --------- Watchers --------
+
+  // ------- Lifecycle ---------
+  constructor() {
+    super();
+  }
+  // --------- Methods ---------
+}
+</script>
+<!----------------- END JS/TS --------------------->
+
+<!----------------- BEGIN HTML -------------------->
+<template lang="html">
+  <div class="v-plan-list">
+    <h1>v-plan-list component works!</h1>
+  </div>
+</template>
+<!----------------- END HTML ---------------------->
+
+<!----------------- BEGIN CSS/SCSS ---------------->
+<style scoped lang="scss">
+.v-plan-list {
+}
+</style>
+<!----------------- END CSS/SCSS ------------------>
