38e9c5479f171eda42e6457cc6b7e41ad9178a5f
mlpierce22 <mlpierce22@gmail.com>
2020-08-25 13:03:04 -0700

    style camera card

--- src/components/shared/VCameraCard.vue
+++ src/components/shared/VCameraCard.vue
@@ -8,6 +8,8 @@ import { CamResolution } from "@/models";
 export default class VCameraCard extends Vue {
   // ---------- Props ----------
   @Prop() camera!: CamResolution;
+
+  @Prop() isDeletable!: boolean;
   // ------- Local Vars --------
   camCount = 1;
 
@@ -21,6 +23,17 @@ export default class VCameraCard extends Vue {
     min: value => value >= 1 || "Must have at least one."
   };
   // --------- Watchers --------
+
+  @Watch("camCount")
+  moreCams() {
+    this.$emit("update", { field: "numCameras", payload: this.camCount });
+  }
+
+  @Watch("cameraTitle")
+  newSelection() {
+    this.$emit("update", { field: "title", payload: this.cameraTitle });
+  }
+
   constructor() {
     // ------- Lifecycle ---------
     super();
@@ -35,6 +48,7 @@ export default class VCameraCard extends Vue {
 <!----------------- BEGIN HTML -------------------->
 <template lang="html">
   <div class="v-camera-card">
+    <v-icon class="icon" color="secondary" size="60">mdi-video-outline</v-icon>
     <div class="resolution">
       <v-select
         :items="camera.cameraOpts"
@@ -56,10 +70,15 @@ export default class VCameraCard extends Vue {
         color="#50B536"
         min="1"
         step="1"
+        outlined
+        :hide-details="true"
         :validate-on-blur="true"
         :rules="[rules.empty, rules.min]"
       ></v-text-field>
     </div>
+    <v-btn icon color="error" :disabled="!isDeletable" @click="$emit('delete')">
+      <v-icon>mdi-trash-can</v-icon>
+    </v-btn>
   </div>
 </template>
 <!----------------- END HTML ---------------------->
@@ -67,6 +86,63 @@ export default class VCameraCard extends Vue {
 <!----------------- BEGIN CSS/SCSS ---------------->
 <style scoped lang="scss">
 .v-camera-card {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  max-width: 650px;
+  background: rgba(203, 227, 196, 0.22);
+  border: 3px solid #50b536;
+  border-radius: 10px;
+  padding: 10px 20px;
+
+  @media only screen and (max-width: 450px) {
+    .icon {
+      display: none;
+    }
+  }
+  .resolution {
+    padding: 0px 10px;
+  }
+
+  .num-streams {
+    display: flex;
+    flex-direction: column;
+    max-width: 119px;
+
+    @media only screen and (max-width: 450px) {
+      .title {
+        font-size: 1.1rem !important;
+      }
+    }
+
+    ::v-deep.text-box {
+      .v-input__slot {
+        border-radius: 10px;
+        min-height: 0px;
+
+        input {
+          text-align: center;
+          padding: 8px 0px 8px 13px;
+
+          @media only screen and (max-width: 450px) {
+            padding: 8px 0px 8px 0px;
+          }
+        }
+      }
+    }
+
+    // ::v-deep .text-box input {
+    //   text-align: center;
+    // }
+    // ::v-deep .v-text-field input {
+    //   padding: 8px 0px 8px 13px;
+    // }
+
+    // ::v-deep .text-box .v-input__slot {
+    //   border-top-right-radius: 10px;
+    //   border-top-left-radius: 10px;
+    // }
+  }
 }
 </style>
 <!----------------- END CSS/SCSS ------------------>
