6eebd4fba2ef7199e4c3e2b5e85bafea1ca69dc9
Mason Pierce <masonpierce@Masons-MacBook-Pro-3.local>
2020-09-01 11:59:26 -0700

    fix styling of back, next buttons

--- src/components/shared/VBackNextButton.vue
+++ src/components/shared/VBackNextButton.vue
@@ -19,11 +19,24 @@ export default class VBackNextButton extends Vue {
 <!----------------- BEGIN HTML ----------------->
 <template lang="html">
   <div class="v-back-next-button">
-    <v-btn class="back-button" v-if="back" @click="$emit('back-click')">
-      {{ back }}
+    <v-btn
+      class="back-button"
+      color="secondary"
+      text
+      v-if="back"
+      @click="$emit('back-click')"
+    >
+      <v-icon>mdi-arrow-left</v-icon> {{ back }}
     </v-btn>
-    <v-btn class="next-button" v-if="next" @click="$emit('next-click')">
-      {{ next }}
+    <v-btn
+      class="next-button"
+      dark
+      depressed
+      color="secondary"
+      v-if="next"
+      @click="$emit('next-click')"
+    >
+      {{ next }} <v-icon>mdi-arrow-right</v-icon>
     </v-btn>
   </div>
 </template>
@@ -31,5 +44,21 @@ export default class VBackNextButton extends Vue {
 <!----------------- BEGIN CSS/SCSS ----------------->
 <style scoped lang="scss">
 .v-back-next-button {
+  display: flex;
+  margin-top: 30px;
+
+  .back-button {
+    .v-icon {
+      margin-right: 5px;
+    }
+  }
+
+  .next-button {
+    margin-left: auto;
+
+    .v-icon {
+      margin-left: 5px;
+    }
+  }
 }
 </style>
