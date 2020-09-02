892dafda8695fa8cc0f4f4bea97a6ac655f95d9b
Mason Pierce <masonpierce@Masons-MacBook-Pro-3.local>
2020-09-02 12:16:26 -0700

    simplify add locations

--- src/components/TheAddLocationsPage.vue
+++ src/components/TheAddLocationsPage.vue
@@ -1,14 +1,10 @@
 <!----------------- BEGIN JS/TS ------------------->
 <script lang="ts">
 import { Component, Vue, Prop, Watch } from "vue-property-decorator";
-import {
-  Plan,
-  DeconstructedHashPlan,
-  Location,
-  QuoteIntroForm
-} from "@/models";
+import { Plan, DeconstructedHashPlan, QuoteIntroForm } from "@/models";
 import VPlanList from "@/components/shared/VPlanList.vue";
 import VLocationCameraDropdown from "@/components/shared/VLocationCameraDropdown.vue";
+import { LocationHashes } from "@/new-models";
 @Component({
   components: {
     VPlanList,
@@ -21,51 +17,45 @@ export default class TheAddLocationsPage extends Vue {
 
   @Prop() initialFormData!: QuoteIntroForm;
 
-  //TODO: get current locations too because otherwise we reset on vertical
+  @Prop() locations!: LocationHashes;
   // ------- Local Vars --------
 
   // currentPlans: Array<DeconstructedHashPlan> | [] = [];
 
-  furthestLocation = 0;
-
-  locations: Location = {};
+  // furthestLocation = 0;
 
   // --------- Watchers --------
-  @Watch("locations", { deep: true })
-  locationChanged() {
-    this.$emit("update-location-app", this.locations);
-  }
 
-  @Watch("initialFormData.numLANLocations", { deep: true })
-  formDataChange(event) {
-    console.log("reinitializing", event);
-    if (event.value > this.furthestLocation) {
-      // start at furthest and iterate until closest
-      for (let i = this.furthestLocation; i < event.value; ++i) {
-        this.createLocation();
-      }
-    } else if (event.value < this.furthestLocation) {
-      Object.keys(this.locations).forEach(key => {
-        if (key > event) {
-          this.deleteLocation(key);
-        }
-      });
-      // splice out the elements from the event till the end
-    } else {
-      // they are equal
-    }
-    // TODO: We want to update this lan locations when we do the add locations button, but we would have an infinite loop here if we did that (what if we just added if there were more?) slice if there are too many, add extras if there arent
-    // for (let i = 1; i <= this.initialFormData["numLANLocations"].value; i++) {
-    //   this.furthestLocation = i;
-    //   this.$set(this.locations, i, {
-    //     title: "Location " + i,
-    //     numCameras: 1,
-    //     planCounts: { 1: 1 }, // set some default so the empty card displays
-    //     useVM: false
-    //   });
-    // }
-    //this.$emit("update-location-app", this.locations);
-  }
+  // @Watch("initialFormData.numLANLocations", { deep: true })
+  // formDataChange(event) {
+  //   console.log("reinitializing", event);
+  //   if (event.value > this.furthestLocation) {
+  //     // start at furthest and iterate until closest
+  //     for (let i = this.furthestLocation; i < event.value; ++i) {
+  //       this.createLocation();
+  //     }
+  //   } else if (event.value < this.furthestLocation) {
+  //     Object.keys(this.locations).forEach(key => {
+  //       if (key > event) {
+  //         this.deleteLocation(key);
+  //       }
+  //     });
+  //     // splice out the elements from the event till the end
+  //   } else {
+  //     // they are equal
+  //   }
+  // TODO: We want to update this lan locations when we do the add locations button, but we would have an infinite loop here if we did that (what if we just added if there were more?) slice if there are too many, add extras if there arent
+  // for (let i = 1; i <= this.initialFormData["numLANLocations"].value; i++) {
+  //   this.furthestLocation = i;
+  //   this.$set(this.locations, i, {
+  //     title: "Location " + i,
+  //     numCameras: 1,
+  //     planIds: { 1: 1 }, // set some default so the empty card displays
+  //     useVM: false
+  //   });
+  // }
+  //this.$emit("update-location-app", this.locations);
+  // }
   // ------- Lifecycle ---------
   constructor() {
     super();
@@ -74,11 +64,11 @@ export default class TheAddLocationsPage extends Vue {
     //   this.$set(this.locations, i, {
     //     title: "Location " + i,
     //     numCameras: 1,
-    //     planCounts: { 1: 1 }, // set some default so the empty card displays
+    //     planIds: { 1: 1 }, // set some default so the empty card displays
     //     useVM: false
     //   });
     // }
-    console.log("called!");
+    // console.log("called!");
   }
 
   // --------- Methods ---------
@@ -95,10 +85,11 @@ export default class TheAddLocationsPage extends Vue {
     const assignedPlans = Object.keys(this.plans).map(planKey => {
       return {
         planId: planKey,
-        sumAssigned: Object.keys(this.locations)
+        field: "camerasAssigned",
+        payload: Object.keys(this.locations)
           .map(locKey => {
-            return this.locations[locKey].planCounts[planKey]
-              ? this.locations[locKey].planCounts[planKey]
+            return this.locations[locKey].planIds[planKey]
+              ? this.locations[locKey].planIds[planKey]
               : 0;
           })
           .reduce((prev, curr) => {
@@ -106,22 +97,23 @@ export default class TheAddLocationsPage extends Vue {
           })
       };
     });
-    this.$emit("assigned-plans-updated", assignedPlans);
+    this.$emit("modify-plan", assignedPlans);
 
-    // update the num camera counts
+    // update the num camera counts in location
     Object.keys(this.locations).map(key => {
-      const totalLocCams = Object.keys(this.locations[key].planCounts)
+      const totalLocCams = Object.keys(this.locations[key].planIds)
         .map(planKey => {
-          return this.locations[key].planCounts[planKey];
+          return this.locations[key].planIds[planKey];
         })
         .reduce((prev, curr) => {
           return prev + curr;
         });
-      console.log(
-        "this should be a number representing all the location's cameras added up!",
-        totalLocCams
-      );
-      this.$set(this.locations[key], "numCameras", totalLocCams);
+      this.$emit("modify-location", {
+        index: parseInt(key),
+        field: "numCameras",
+        payload: totalLocCams
+      });
+      // this.$set(this.locations[key], "numCameras", totalLocCams);
     });
 
     // for each location, iterate through plans and add them to overall count, once iterated through all plans, set num cameras equal to result
@@ -131,26 +123,31 @@ export default class TheAddLocationsPage extends Vue {
   }
 
   createLocation() {
-    this.furthestLocation++;
-    const newLocNum = this.furthestLocation;
-    this.$set(this.locations, newLocNum, {
-      title: "Location " + newLocNum,
-      numCameras: 1,
-      planCounts: { 1: 1 }, // set some default
-      useVM: false
-    });
+    this.$emit("add-location");
+    // this.furthestLocation++;
+    // const newLocNum = this.furthestLocation;
+    // this.$set(this.locations, newLocNum, {
+    //   title: "Location " + newLocNum,
+    //   numCameras: 1,
+    //   planIds: { 1: 1 }, // set some default
+    //   useVM: false
+    // });
 
-    this.$emit("location-count-change", Object.keys(this.locations).length);
+    // this.$emit("add-location", Object.keys(this.locations).length);
   }
 
-  deleteLocation(index) {
-    //TODO: is this necessary?
-    this.$delete(this.locations, index);
-    this.$emit("location-count-change", Object.keys(this.locations).length);
+  deleteLocation(index: number) {
+    // this.$delete(this.locations, index);
+    this.$emit("delete-location", index);
+    // this.$emit("location-count-change", Object.keys(this.locations).length);
   }
-
+  // TODO: is the plan cam count in the location updated??
   updateLocation(index, payload) {
-    this.$set(this.locations[index], payload.field, payload.payload);
+    this.$emit("modify-location", {
+      index: parseInt(index),
+      field: payload.field,
+      payload: payload.payload
+    });
   }
 
   openEditModal() {
