e088271f819884b737b6e399b8f32d0a738bd32b
Mason Pierce <mlpierce22@gmail.com>
2020-09-21 12:35:14 -0700

    deploy recent changes for github pages

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@ module.exports = {
   configureWebpack: {
     devtool: "source-map"
   },
-  
-  //publicPath: "/camio-price-tool/", // required if we want to use github
-  outputDir: "dist" // doc will allow it to be served by github
+
+  publicPath: "/camio-price-tool/", // required if we want to use github
+  outputDir: "docs" // doc will allow it to be served by github
 };
